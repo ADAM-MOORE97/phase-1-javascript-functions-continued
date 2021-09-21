d3ba08cb565292a92cf474200b49cbb6217b562c
Adam <adammoore118@outlook.com>
2021-09-21 17:08:16 -0400

    commit tree 29b50ad446e589311093167979a62f817ed52602

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Your code here
+function saturdayFun(value) {
+    if (value==="bathe my dog"){
+        return "This Saturday, I want to bathe my dog!"
+    }else{
+    return "This Saturday, I want to roller-skate!";
+    }
+}
+
+// saturdayFun = (value) => {
+//     if(value==="bathe my dog"){
+//         return "This Saturday, I want to bathe my dog!"
+//     } else {
+//         return "This Saturday, I want to roller-skate!"
+//     }
+// }
+
+// mondayWork = (value) => {
+//     if(value==="work from home"){
+//         return "This Monday, I will work from home.";
+//     }
+//     else {
+//         return "This Monday, I will go to the office.";
+//     }
+//  }
+ function mondayWork(value) {
+  if (value==="work from home"){
+       return "This Monday, I will work from home."
+    } else {
+   return "This Monday, I will go to the office." ;  
+ }
+}
+
+// function wrapAdjective(value){
+//     let result = wrapAdjective(value)
+//     let emphatic = result(value)
+
+//     if(value==='*'){
+//         return 'You are *a hard worker*!'
+// } else if(value==='||'){
+//     return 'You are ||a dedicated programmer||!'
+// }
+
+
+// }
+
+const wrapAdjective = (flair='*') => {
+    return (adjective) => `You are ${flair}${adjective}${flair}!`;
+  }
